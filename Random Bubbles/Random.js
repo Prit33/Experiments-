d628a9c78850a5a793c1379745dc46e5db87e1b4
Prit <74133659+Prit33@users.noreply.github.com>
2021-05-03 01:01:50 +0530

    Add files via upload

---
+++ Random Bubbles/Random.js
@@ -0,0 +1,30 @@
+var points={x:0,y:0,d:10};
+var col={r:0,g:0,b:0};
+function setup() {
+  createCanvas(1500, 1500);
+  background(0);
+}
+
+function draw() {
+  col.r=random(0,255);
+  col.g=random(0,50);
+  col.b=random(20,255);
+
+  points.x=random(0,width);
+  points.y=random(0,height);
+  points.d=random(10,75);
+
+  // if(points.x >=683)
+  // {
+  //   fill(col.r,col.g,col.b,200);
+  //   noStroke();
+  //   ellipse(points.x,points.y,points.a,points.a);
+
+  // }
+  // else{
+
+  fill(col.r,col.g,col.b,1000);
+  noStroke();
+  ellipse(points.x,points.y,points.d,points.d)
+  
+}
